e099e11d85f6b0c2943ee61a4aa7a85faeba2557
yxsh16 <yash16@duck.com>
2024-09-27 18:43:04 +0530

    functions

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     println!("{}", x + y + z); // Only signed integers can be operated together
 
     let mut a = 32; // This is a mutable variable
-    println!("{}", a + 32);
+    a = a + 33;
+    println!("{}", a);
 
     let is_present: bool = true;
     let is_pass: bool = false;
@@ -26,4 +27,35 @@ fn main() {
         Some(c) => println!("{}", c),
         None => println!("No character at index"),
     }
+
+    // Conditionals
+    let is_even: bool = true;
+
+    if is_even {
+        println!("The num is even");
+    } else if !is_even {
+        println!("The num is odd");
+    }
+
+    // Loops
+    for i in 0..5 {
+        println!("{}", i);
+    }
+
+    // Functions
+
+    let sentence = String::from("This is a sentence");
+    let first_word = get_first_word(sentence);
+    println!("The first word is: {}", first_word);
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut res = String::new();
+    for c in sentence.chars() {
+        if c == ' ' {
+            break;
+        }
+        res.push(c);
+    }
+    return res;
 }
